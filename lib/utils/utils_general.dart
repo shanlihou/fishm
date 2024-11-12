a54c6e2c941ebf67bcf85f4660531bff67509225
shanlihou <shanlihou@gmail.com>
2024-11-12 11:59:37 +0800

    up

--- lib/utils/utils_general.dart
+++ lib/utils/utils_general.dart
@@ -4,7 +4,6 @@ import 'package:archive/archive.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
-import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:yaml/yaml.dart';
