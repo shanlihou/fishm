e5fc8c9875370d73075ed011fdfce70210bd65bc
shanlihou <shanlihou@gmail.com>
2024-11-22 10:21:21 +0800

    up

---
+++ lib/views/pages/reader_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ReaderPage extends StatefulWidget {
+  const ReaderPage({super.key});
+
+  @override
+  State<ReaderPage> createState() => _ReaderPageState();
+}
+
+class _ReaderPageState extends State<ReaderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
